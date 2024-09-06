5adf75d8c530563b0978a98f9bd5d219e09fe196
Roland Hughes <roland@logikalsolutions.com>
2024-09-06 09:21:57 -0500

    Added comment for Ubuntu 18
    Different CMake package needed for Ubuntu 18 as the main repos have too old a version.

--- ls-cs-Deb-build-dependencies.sh
+++ ls-cs-Deb-build-dependencies.sh
@@ -6,7 +6,9 @@ apt-get install libfreetype6-dev libfontconfig1-dev libglib2.0-dev libgstreamer1
 apt-get install libxcb1-dev libx11-xcb-dev libxcb-glx0-dev libxcb-icccm4-dev libxcb-image0-dev libxcb-keysyms1-dev \
                 libxcb-render0-dev libxcb-render-util0-dev libxcb-randr0-dev libxcb-shape0-dev \
                 libxcb-shm0-dev libxcb-sync-dev libxcb-xfixes0-dev libxcb-xinerama0-dev libxcb-xkb-dev
-
+#
+#   NOTE: Ubuntu 18.04 users will need cmake-mozilla and cmake_mozilla-data 3.16.3 or higher
+#
 apt-get install fakeroot hashdeep dpkg-dev cmake ninja-build g++ build-essential \
 mercurial mercurial-common libcups2-dev libasound2-dev libxml++2.6-dev libssl-dev libpulse-dev git astyle \
 hunspell libhunspell-dev libpqxx-dev libpq-dev unixodbc-dev libmysql++-dev tree
