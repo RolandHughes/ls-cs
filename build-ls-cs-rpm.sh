8fa1b3b13758715e9ec88bbaf9fbe8db6dfa5633
Roland Hughes <roland@logikalsolutions.com>
2024-09-07 16:12:59 -0500

    RPM build cleanup
    Just some cleanup for RPM builds

--- build-ls-cs-rpm.sh
+++ build-ls-cs-rpm.sh
@@ -20,7 +20,7 @@ echo ""
 echo "This script ASSUMES it can create and use copperspice_rpm_build"
 echo "directory one level up from where this script is being run. If directory"
 echo "exist it will be deleted and recreated. The only reason the build directory is"
-echo "created is so cmake can populate variable values in the copperspice.spec file."
+echo "created is so cmake can populate variable values in the ls-cs.spec file."
 echo ""
 echo "Script also ASSUMES you are running from the root of the Git project directory"
 echo "where all source is in a directory named src at the same level as this file."
@@ -56,7 +56,7 @@ mkdir -p "$BUILD_DIR"
 pushd `pwd`
 
 #  Step 3 : Prepare build directory
-#           We do this just to get copperspice.spec with all of the CMake variables filled in.
+#           We do this just to get ls-cs.spec with all of the CMake variables filled in.
 #
 echo "*** Prepping build directory"
 cd "$BUILD_DIR"
@@ -68,7 +68,7 @@ cmake -G "Ninja" -DCMAKE_BUILD_TYPE=Release \
 
 #  Step 4 : build copperpice and generate the RPM
 #
-rpmbuild -ba copperspice.spec
+rpmbuild -ba ls-cs.spec
 
 echo ""
 echo "If this completed without errors you will find the RPM in {$HOME}/rpmbuild/RPMS"
--- ls-cs-RPM-build-dependencies.sh
+++ ls-cs-RPM-build-dependencies.sh
@@ -1,8 +1,8 @@
-zypper install -t pattern devel_basis
+zypper install -t pattern devel_basis   
 
 # by default only version 7 is installed and you need at least 11 for Scintilla
 #
-zypper install cpp11 gcc11 gcc11-c++
+zypper install cpp11 gcc11 gcc11-c++ jed jed-common emacs
 
 zypper install freetype-devel fontconfig-devel glib2-devel gstreamer-devel gstreamer-plugins-base-devel \
             libICE-devel Mesa-devel Mesa-libGL-devel glibc-devel libtirpc-devel libnsl2 libSM-devel \
@@ -19,5 +19,22 @@ zypper install fakeroot hashdeep dpkg-dev cmake ninja \
             mercurial libcups2 libasound2 libxml++-devel libopenssl-devel libpulse-devel git astyle \
             hunspell hunspell-devel libpqxx-devel  unixODBC-devel libmysqlcppconn-devel rpmdevtools \
             rpm-build postgresql-devel tree
+
+# By default OpenSuSE installs version 7 of C/C++ compilers. You need
+# at least version 11 to work with newer libraries.
+#
+# Other distros may need to run these as well
+#
+if [[ $MACHTYPE == *"suse"* ]]; then
+    update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-11 50
+    update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-7 20
+    update-alternatives --install /usr/bin/cpp cpp /usr/bin/cpp-11 50
+    update-alternatives --install /usr/bin/cpp cpp /usr/bin/cpp-7 20
+    update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-11 50
+    update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-7 20
+    update-alternatives --config g++
+    update-alternatives --config gcc
+    update-alternatives --config cpp
+fi
 
 # reboot
