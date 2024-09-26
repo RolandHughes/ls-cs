515e3ed830f9df52fbc852bc1a53a04ed0761a19
Roland Hughes <roland@logikalsolutions.com>
2024-09-26 08:01:46 -0500

    Pushing to Ubuntu 20.04 for cross compile
    Cannot cross compile on Debian 12. At least not with traditional workflow.

--- LsCs-Deb-build-dependencies.sh
+++ LsCs-Deb-build-dependencies.sh
@@ -1,14 +1,34 @@
-apt-get install libfreetype6-dev libfontconfig1-dev libglib2.0-dev libgstreamer1.0-dev \
-                libgstreamer-plugins-base1.0-dev libice-dev libaudio-dev libgl1-mesa-dev libc6-dev \
-                libsm-dev libxcursor-dev libxext-dev libxfixes-dev libxi-dev libxinerama-dev \
-                libxrandr-dev libxrender-dev libxkbcommon-dev libxkbcommon-x11-dev libx11-dev
+#!/bin/bash
+#
+# LsCs-Deb-build-dependencies.sh
+#
+if [ `whoami` != root ]; then
+    echo Please run this script as root or using sudo
+    exit
+fi
 
-apt-get install libxcb1-dev libx11-xcb-dev libxcb-glx0-dev libxcb-icccm4-dev libxcb-image0-dev libxcb-keysyms1-dev \
-                libxcb-render0-dev libxcb-render-util0-dev libxcb-randr0-dev libxcb-shape0-dev \
-                libxcb-shm0-dev libxcb-sync-dev libxcb-xfixes0-dev libxcb-xinerama0-dev libxcb-xkb-dev
+
+# Executables first
+#
+
+apt-get install fakeroot hashdeep dpkg-dev cmake ninja-build mercurial mercurial-common git astyle \
+        hunspell tree
+
+# this list will need to be cleaned up once GLFW is the only backend
+apt-get install libfreetype6-dev libfontconfig1-dev libglib2.0-dev libgstreamer1.0-dev \
+        libgstreamer-plugins-base1.0-dev libice-dev libaudio-dev libgl1-mesa-dev \
+        libc6-dev libsm-dev libxcursor-dev libxext-dev libxfixes-dev \
+        libxi-dev libxinerama-dev libxrandr-dev libxrender-dev libxkbcommon-dev \
+        libxkbcommon-x11-dev libx11-dev
+
+apt-get install libxcb1-dev libx11-xcb-dev libxcb-glx0-dev libxcb-icccm4-dev \
+        libxcb-image0-dev libxcb-keysyms1-dev libxcb-render0-dev libxcb-render-util0-dev \
+        libxcb-randr0-dev libxcb-shape0-dev libxcb-shm0-dev libxcb-sync-dev \
+        libxcb-xfixes0-dev libxcb-xinerama0-dev libxcb-xkb-dev
 #
 #   NOTE: Ubuntu 18.04 users will need cmake-mozilla and cmake_mozilla-data 3.16.3 or higher
 #
-apt-get install fakeroot hashdeep dpkg-dev cmake ninja-build g++ build-essential \
-mercurial mercurial-common libcups2-dev libasound2-dev libxml++2.6-dev libssl-dev libpulse-dev git astyle \
-hunspell libhunspell-dev libpqxx-dev libpq-dev unixodbc-dev libmysql++-dev tree libvulkan-dev
+apt-get install g++ build-essential libcups2-dev libasound2-dev libxml++2.6-dev \
+        libssl-dev libpulse-dev libhunspell-dev libpqxx-dev libpq-dev unixodbc-dev \
+        libmysql++-dev libvulkan-dev
+
