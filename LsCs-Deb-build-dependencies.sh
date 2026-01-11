4725ac0ff74021dbe7b258de20938bc5a8068466
Roland Hughes <roland@logikalsolutions.com>
2026-01-11 16:18:20 -0600

    kicking vcpkg to curb
    Will use for windows and nothing else

--- LsCs-Deb-build-dependencies.sh
+++ LsCs-Deb-build-dependencies.sh
@@ -7,41 +7,44 @@
 #
 
 sudo apt-get install -y build-essential g++ fakeroot hashdeep dpkg-dev ninja-build \
-     mercurial mercurial-common git astyle tree python3-distutils \
-     python3-distutils-extra autotools-dev libtool-bin bison flex nasm hunspell
+     mercurial mercurial-common git astyle tree 
 
 # we use apostrophe to edit Markdown documentation like README.md
 #
 ./install-apostrophe.sh
 
-# with 0.2.1 package builing was migrated to vcpkg.
+# this list will need to be cleaned up once GLFW is the only backend
+sudo apt-get install libfreetype6-dev libfontconfig1-dev libglib2.0-dev libgstreamer1.0-dev \
+        libgstreamer-plugins-base1.0-dev libice-dev libaudio-dev libgl1-mesa-dev \
+        libc6-dev libsm-dev libxcursor-dev libxext-dev libxfixes-dev \
+        libxi-dev libxinerama-dev libxrandr-dev libxrender-dev libxkbcommon-dev \
+        libxkbcommon-x11-dev libx11-dev
+
+sudo apt-get install libxcb1-dev libx11-xcb-dev libxcb-glx0-dev libxcb-icccm4-dev \
+        libxcb-image0-dev libxcb-keysyms1-dev libxcb-render0-dev libxcb-render-util0-dev \
+        libxcb-randr0-dev libxcb-shape0-dev libxcb-shm0-dev libxcb-sync-dev \
+        libxcb-xfixes0-dev libxcb-xinerama0-dev libxcb-xkb-dev
 #
-./install-vcpkg.sh
-
-# sadly vcpkg needs versions of autoconf stuff that aren't in distros
+#   NOTE: Ubuntu 18.04 users will need cmake-mozilla and cmake_mozilla-data 3.16.3 or higher
 #
-sudo ./install-autotools.sh
+sudo apt-get install g++ build-essential libcups2-dev libasound2-dev libxml++2.6-dev \
+        libssl-dev libpulse-dev libhunspell-dev unixodbc-dev libmysql++-dev libvulkan-dev \
+        libsqlite3-dev
 
-# libraries we cannot get via vcpkg
-#
-sudo apt-get install -y libcups2-dev libc6-dev 
+# to build with MySQL/MariaDB support uncomment the following
+# sudo apt-get libmysqlclient-dev libmariadb-dev libmariadbclient-dev
 
-# libaudio-dev libxcursor-dev libxext-dev libxfixes-dev libxkbcommon-x11-dev 
-# this list will need to be cleaned up once SDL3 is the only backend
+echo "To build Postgresql support you need to install the postgresql-server-dev-nn package for your distro"
+echo "it will install a ton of dependencies.  If you want to actually use Postgresql then uncomment "
+echo "the following line: "
+echo "#sudo apt-get install postgresql postgresql-client postgresql-contrib"
 
-# apt-get install libxcb1-dev libx11-xcb-dev libxcb-glx0-dev libxcb-icccm4-dev \
-#         libxcb-image0-dev libxcb-keysyms1-dev libxcb-render0-dev libxcb-render-util0-dev \
-#         libxcb-randr0-dev libxcb-shape0-dev libxcb-shm0-dev libxcb-sync-dev \
-#         libxcb-xfixes0-dev libxcb-xinerama0-dev libxcb-xkb-dev
-
-echo "***************************************************************"
-echo "***************************************************************"
-echo "***************************************************************"
 echo " "
-echo "vcpkg was intalled in  $HOME/vcpkg"
-echo "you need to add "
-echo "        export VCPKG_ROOT=$HOME/vcpkg"
-echo "to either ~/.profile  or ~/.bashrc"
+echo " "
+echo "For Ubuntu 20.04   sudo apt-get install postgresql-server-dev-12"
+echo "For Ubuntu 22.04   sudo apt-get install postgresql-server-dev-14"
+echo "For Ubuntu 24.04   sudo apt-get install postgresql-server-dev-16"
+echo " "
 echo " "
 echo "***************************************************************"
 echo "***************************************************************"
